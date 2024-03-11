cdc93323ac55ae2fd00b3e14d571d9353627886b
Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2024-03-11 23:10:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(page_title="Farhan Q&A Bot")
 st.title("Farhan GPT 🤖")
 st.subheader("AI Assistant 🥷")
 
-input=st.text_area("Prompt: ",key="Write prompt")
+input=st.chat_input("Prompt: ",key="Write prompt")
 submit=st.button("Generate")
 
 ## Whwn submit is clicked
