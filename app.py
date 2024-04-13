1b43fd84bd79ed7c4cac649a67ca3dc11627efd6
Muhammad Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2024-04-13 15:20:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 # function to load Gemini Pro model and get reponses
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-pro-latest")
 
 def get_gemini_response(question):
     response = model.generate_content(question)
